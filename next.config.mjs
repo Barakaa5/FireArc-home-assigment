cdf6eb05a8d01d03a139ac0cc6ebb92119033615
Barak Amzaleg <amzalegbarak@gmail.com>
2024-05-17 17:06:23 +0300

    revert

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Ensure the JSON file is included in the server build
-      config.module.rules.push({
-        test: /\.json$/,
-        type: "javascript/auto",
-        use: [{ loader: "file-loader", options: { name: "[name].[ext]" } }],
-      });
-    }
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
