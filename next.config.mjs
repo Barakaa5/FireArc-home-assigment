6e11ac0a342135447d939e81b55b9d34ef65c8fa
Barak Amzaleg <amzalegbarak@gmail.com>
2024-05-17 17:05:21 +0300

    try

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Ensure the JSON file is included in the server build
+      config.module.rules.push({
+        test: /\.json$/,
+        type: "javascript/auto",
+        use: [{ loader: "file-loader", options: { name: "[name].[ext]" } }],
+      });
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
